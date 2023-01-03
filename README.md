ef3cb093a4ebdb67a26bdddd2663204dfd152643
Øyvind Raddum Berg <elacin@gmail.com>
2023-01-03 23:56:08 +0100

    bleep setup

--- README.md
+++ README.md
@@ -1,48 +1,13 @@
-# Coursier setup GitHub Action
+# Bleep setup GitHub Action
 
-A GitHub Action to install Coursier and use it to install Java and Scala CLI tools.
+A GitHub Action to install [bleep](https://bleep.build).
 
-It can be useful if you want to install a specific version of JVM or use a build tool like `mill` or `seed`.
-
-Inspired by [olafurpg/setup-scala](https://github.com/olafurpg/setup-scala) and the blog post [Single command Scala setup](https://alexarchambault.github.io/posts/2020-09-21-cs-setup.html) by Alex Archambault (author of Coursier).
+Forked from [coursier/setup-action](https://github.com/coursier/setup-action), 
+which was inspired by [olafurpg/setup-scala](https://github.com/olafurpg/setup-scala) 
+and the blog post [Single command Scala setup](https://alexarchambault.github.io/posts/2020-09-21-cs-setup.html) by Alex Archambault (author of Coursier).
 
 ## Features
 
-- run it on any platform: Linux, MacOS, Windows
-- install [any JVM](https://get-coursier.io/docs/cli-java.html#jvm-index) you need
-- setup the build tool of your choice: sbt, mill, seed, etc.
-- install other common Scala CLI tools: Ammonite, Bloop, giter8, [etc.](https://github.com/coursier/apps/tree/master/apps/resources)
+- run bleep on any platform: Linux, MacOS, Windows
+- will install the version you have configured in bleep.yaml
 
-## Inputs
-
-- `jvm` (optional): JVM to install
-  - one of the options from `cs java --available`.
-  - if left empty either the existing JVM will be used or Coursier will install its default JVM.
-- `apps` (optional): Scala apps to install (`sbtn` by default)
-  - space separated list of app names (from the [main channel](https://github.com/coursier/apps))
-
-### Example with custom inputs
-
-```yml
-  steps:
-    - uses: actions/checkout@v2
-    - uses: coursier/setup-action@v1
-      with:
-        jvm: adopt:11
-        apps: sbtn bloop ammonite
-```
-
-## Outputs
-
-- `cs-version`: version of the installed Coursier (should be the latest available)
-
-## Caching
-
-This action should work well with the official Coursier [cache-action](https://github.com/coursier/cache-action). For example:
-
-```yml
-  steps:
-    - uses: actions/checkout@v2
-    - uses: coursier/cache-action@v6
-    - uses: coursier/setup-action@v1
-```
--- src/main.ts
+++ src/main.ts
@@ -1,111 +1,50 @@
-import * as cli from '@actions/exec'
 import * as core from '@actions/core'
-import * as os from 'os'
-import * as path from 'path'
+import * as fs from 'fs'
 import * as tc from '@actions/tool-cache'
+import * as yaml from 'yaml'
+import path from 'path'
 
-let csVersion = core.getInput('version')
-if (!csVersion) csVersion = '2.1.0-M7-39-gb8f3d7532'
+async function downloadBleep(version: string): Promise<string> {
+  const baseUrl = `https://github.com/oyvindberg/bleep/releases/download/v${version}/bleep-`
 
-const coursierVersionSpec = csVersion
-
-async function execOutput(cmd: string, ...args: string[]): Promise<string> {
-  let output = ''
-  const options = {
-    listeners: {
-      stdout: (data: Buffer) => {
-        output += data.toString()
-      },
-    },
+  const unixLike = async (url: string): Promise<string> => {
+    const guid = await tc.downloadTool(url)
+    const extracted = await tc.extractTar(guid)
+    const cachedFile = await tc.cacheFile(extracted, 'bleep', 'bleep', version)
+    fs.chmodSync(cachedFile, '+x')
+    core.addPath(path.dirname(cachedFile))
+    return cachedFile
   }
-  await cli.exec(cmd, args.filter(Boolean), options)
-  return output.trim()
-}
 
-async function downloadCoursier(): Promise<string> {
-  const baseUrl = `https://github.com/coursier/coursier/releases/download/v${csVersion}/cs-x86_64`
-  let csBinary = ''
-  switch (process.platform) {
-    case 'linux': {
-      const guid = await tc.downloadTool(`${baseUrl}-pc-linux.gz`)
-      const arc = `${guid}.gz`
-      await cli.exec('mv', [guid, arc])
-      csBinary = arc
-      break
-    }
-    case 'darwin': {
-      const guid = await tc.downloadTool(`${baseUrl}-apple-darwin.gz`)
-      const arc = `${guid}.gz`
-      await cli.exec('mv', [guid, arc])
-      csBinary = arc
-      break
-    }
-    case 'win32': {
-      const guid = await tc.downloadTool(`${baseUrl}-pc-win32.zip`)
-      const arc = `${guid}.zip`
-      await cli.exec('mv', [guid, arc])
-      csBinary = arc
-      break
-    }
-    default:
-      core.setFailed(`Unknown process.platform: ${process.platform}`)
-  }
-  if (!csBinary) core.setFailed(`Couldn't download Coursier`)
-  if (csBinary.endsWith('.gz')) {
-    await cli.exec('gzip', ['-d', csBinary])
-    csBinary = csBinary.slice(0, csBinary.length - '.gz'.length)
-  }
-  if (csBinary.endsWith('.zip')) {
-    const destDir = csBinary.slice(0, csBinary.length - '.zip'.length)
-    await cli.exec('unzip', ['-j', csBinary, 'cs-x86_64-pc-win32.exe', '-d', destDir])
-    csBinary = `${destDir}\\cs-x86_64-pc-win32.exe`
-  }
-  await cli.exec('chmod', ['+x', csBinary])
-  return csBinary
-}
-
-async function cs(...args: string[]): Promise<string> {
-  const previous = tc.find('cs', coursierVersionSpec)
-  if (previous) {
-    core.addPath(previous)
+  if (process.platform === 'linux' && process.arch === 'x64') {
+    return unixLike(`${baseUrl}-x86_64-pc-linux.tar.gz`)
+  } else if (process.platform === 'win32' && process.arch === 'x64') {
+    const guid = await tc.downloadTool(`${baseUrl}-x86_64-pc-win32.zip`)
+    const extracted = await tc.extractZip(guid)
+    const cachedFile = await tc.cacheFile(extracted, 'bleep', 'bleep', version)
+    core.addPath(path.dirname(cachedFile))
+    return cachedFile
+  } else if (process.platform === 'darwin' && process.arch === 'x64') {
+    return unixLike(`${baseUrl}-x86_64-apple-darwin.tar.gz`)
+  } else if (process.platform === 'darwin' && process.arch === 'arm64') {
+    return unixLike(`${baseUrl}-arm64-apple-darwin.tar.gz`)
   } else {
-    const csBinary = await downloadCoursier()
-    const binaryName = process.platform === 'win32' ? 'cs.exe' : 'cs'
-    const csCached = await tc.cacheFile(csBinary, binaryName, 'cs', csVersion)
-    core.addPath(csCached)
+    return Promise.reject(new Error(`Not supported os/arch: ${process.platform}/${process.arch}`))
   }
-  return execOutput('cs', ...args)
 }
 
 async function run(): Promise<void> {
   try {
-    await core.group('Install Coursier', async () => {
-      await cs('--help')
-      core.setOutput('cs-version', csVersion)
-    })
-
-    await core.group('Install JVM', async () => {
-      const jvmInput = core.getInput('jvm')
-      const jvmArg = jvmInput ? ['--jvm', jvmInput] : []
-      if (!jvmInput && process.env.JAVA_HOME) {
-        core.info(`skipping, JVM is already installed in ${process.env.JAVA_HOME}`)
-      } else {
-        await cs('java', ...jvmArg, '-version')
-        const csJavaHome = await cs('java-home', ...jvmArg)
-        core.exportVariable('JAVA_HOME', csJavaHome)
-        core.addPath(path.join(csJavaHome, 'bin'))
+    await core.group('Install Bleep', async () => {
+      if (!fs.existsSync('bleep.yaml')) {
+        throw new Error(
+          "bleep.yaml doesn't exist. You likely need to run `bleep-setup-action` after checking out source",
+        )
       }
-    })
-
-    await core.group('Install Apps', async () => {
-      const value = core.getInput('apps').trim()
-      const apps: string[] = value.split(' ')
-      if (value && apps.length) {
-        const coursierBinDir = path.join(os.homedir(), 'cs', 'bin')
-        core.exportVariable('COURSIER_BIN_DIR', coursierBinDir)
-        core.addPath(coursierBinDir)
-        await cs('install', '--contrib', ...apps)
-      }
+      const bleepYamlStr = fs.readFileSync('bleep.yaml', { encoding: 'utf-8' })
+      const bleepYaml = yaml.parse(bleepYamlStr)
+      const version = bleepYaml['$version']
+      await downloadBleep(version)
     })
   } catch (error: unknown) {
     const msg = error instanceof Error ? error.message : String(error)
